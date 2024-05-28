368cc6b4c9699ac50ae2e93865ee41d7462d88a7
hexadecimals19 <randy2016andman@gmail.com>
2024-05-28 13:31:25 +0800

    LandingPage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 * Soft UI Design System - v1.0.9
 =========================================================
 
-* Product Page:  https://www.creative-tim.com/product/soft-ui-design-system 
+* Product Page:  https://www.creative-tim.com/product/soft-ui-design-system
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 * Coded by www.creative-tim.com
 
@@ -18,9 +18,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('softui/assets/img/apple-icon.png') }}">
-  
+
   <link rel="icon" type="image/png" href="{{ asset('softui/assets/img/favicon.png') }}">
-  
+
 
   <title>
     Soft UI Design System by Creative Tim
@@ -31,9 +31,9 @@
 
   <!-- Nucleo Icons -->
   <link href="{{ asset('softui/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-  
+
   <link href="{{ asset('softui/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-  
+
 
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
@@ -41,7 +41,7 @@
 
   <!-- CSS Files -->
   <link id="pagestyle" href="{{ asset('softui/assets/css/soft-design-system.css?v=1.0.9') }}" rel="stylesheet" />
-  
+
   <!-- Nepcha Analytics (nepcha.com) -->
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
@@ -61,33 +61,46 @@
                             <img src="{{ asset('softui/assets/img/logo-jilidEnam.png') }}" alt="Logo" style="width:43px; height: 65px;">
                         </div>
                         <!-- Navigation Bar -->
-                        <nav class="navbar navbar-expand-lg">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="#">Menu</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="#">Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="#">Contact</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-black" href="#">Events</a>
-                                </li>
-                            </ul>
-                        </nav>
+                        <nav class="navbar navbar-expand-lg fixed-top">
+                            <div class="container-fluid">
+
+                              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                              </button>
+                              <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav ms-auto">
+                                  <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">Home</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">Menu</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">Login</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">Contact</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">Events</a>
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                          </nav>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-7 text-center mx-auto">
-                    <h1 class="text-white py-3 mt-n5">Jilid Enam</h1>
-                    <button class="bg-danger border-0 px-3 py-2 font-weight-bold text-white border-radius-md">View Menu</button>
+                    <h1 class="text-white py-3 mt-n5">JILID ENAM</h1>
+                    <h5 class="text-white h6">Join us as we continue to explore new flavors and innovate our brewing techniques. At Jilid Enam, every cup tells a story of dedication, expertise, and a relentless pursuit of excellence.
+                    </h5>
+                    <button class="bg-danger border-0 px-3 py-2 font-weight-bold text-white border-radius-md">Login</button>
+                    <button class="bg-danger border-0 px-3 py-2 font-weight-bold text-white border-radius-md">Register</button>
                 </div>
+            </div>
+
             </div>
         </div>
     </div>
@@ -95,54 +108,65 @@
 
 
 
-  <section class="my-5 py-5">
+<section class="my-5 py-5">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col align-self-center">
-          <h5 class="h6">DIVING DEEPERS INTO JILID ENAM'S WORLD</h5>
-          <h5 class="h3">WE SPECIALISE IN GOOD <br>BREWS AND COFFEE</h5>
-          <div class="row justify-content-end">
-            <img src="{{ asset('softui/assets/img/making-dough.png') }}" width="406px" height="567px"></img>
-          </div>
+        <div class="row justify-content-center">
+            <div class="col align-self-center">
+                <h5 class="h6">DIVING DEEPER INTO JILID ENAM'S WORLD</h5>
+                <h5 class="h3">WE SPECIALIZE IN GOOD <br>BREWS AND COFFEE</h5>
+                <div class="row justify-content-end">
+                    <img src="{{ asset('softui/assets/img/making-dough.png') }}" width="406px" height="567px"></img>
+                </div>
+            </div>
+            <div class="col-md">
+                <img src="{{ asset('softui/assets/img/barista-machine.png') }}" width="507px" height="722px"></img>
+                <br>
+                <br>
+                <h5 class="h6">At Jilid Enam, we believe in the art of coffee making. Our journey began over two decades ago in a small corner of the city, where a passion for perfect brews was born. Today, we stand as a testament to quality, consistency, and a deep love for coffee.
+                </h5>
+
+            </div>
         </div>
-        <div class="col-md">
-          <img src="{{ asset('softui/assets/img/barista-machine.png') }}" width="507px" height="722px"></img>
-          <h5 class="h6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              </h5>
-        </div>
+    </div>
+</section>
+
       </div>
-    </div> 
+    </div>
   </section>
 
   <section class="my-5 py-5">
     <div class="container">
-      <div class="row">
-        <div class="row justify-content-center text-center my-sm-5">
-          <div class="col-lg-6">
-            <h2 class="text-dark mb-0">COME AND FEAST</h2>
-            <h2 class="text-primary text-gradient">Delicious Food & Coffee</h2>
-          </div>
-        </div>
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-6 col-md-4">
-          <img src="{{ asset('softui/assets/img/food 1.jpg') }}" width="85%" height="85%"></img>
-          <h6>Lorem Ipsum</h6>
-          <p class="h6">Lorem Ipsum</p>
+        <div class="row">
+            <div class="row justify-content-center text-center my-sm-5">
+                <div class="col-lg-6">
+                    <h2 class="text-dark mb-0">COME AND FEAST</h2>
+                    <h2 class="text-primary text-gradient">Delicious Food & Coffee</h2>
+                </div>
+            </div>
         </div>
 
-        <div class="col-6 col-md-4">
-          <img src="{{ asset('softui/assets/img/food 2.jpg') }}" width="85%" height="85%"></img>
-          <h6>Lorem Ipsum</h6>
-          <p class="h6">Lorem Ipsum</p>
-        </div>
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-4">
+                <img src="{{ asset('softui/assets/img/food 1.jpg') }}" width="85%" height="85%"></img>
+                <h6>Signature Dish</h6>
+                <p class="h6">Experience our famous grilled chicken, seasoned with a unique blend of spices and served with a side of fresh vegetables.</p>
+            </div>
 
-        <div class="col-6 col-md-4">
-          <img src="{{ asset('softui/assets/img/food 3.jpg') }}" width="85%" height="85%"></img>
-          <h6>Lorem Ipsum</h6>
-          <p class="h6">Lorem Ipsum</p>
+            <div class="col-6 col-md-4">
+                <img src="{{ asset('softui/assets/img/food 2.jpg') }}" width="85%" height="85%"></img>
+                <h6>Artisanal Coffee</h6>
+                <p class="h6">Savor the rich and bold flavors of our artisanal coffee, crafted from the finest beans sourced globally.</p>
+            </div>
+
+            <div class="col-6 col-md-4">
+                <img src="{{ asset('softui/assets/img/food 3.jpg') }}" width="85%" height="85%"></img>
+                <h6>Decadent Desserts</h6>
+                <p class="h6">Indulge in our selection of decadent desserts, from creamy cheesecakes to rich chocolate mousse.</p>
+            </div>
         </div>
+    </div>
+</section>
+
       </div>
     </div>
   </section>
@@ -184,7 +208,7 @@
     <div class="container">
       <div class="col-lg-7 text-center mx-auto">
         <h5 class="h1" style="font-family: Amiri, font-size: 200px;">“</h5>
-        <h5 class="h6">Chilly ambience and the place is really nice to hangout or to just read<br>books while sipping some coffee.<br>The staffs are very friendly too, they definitely serve good food and good<br>drinks!</h5>
+        <h5 class="h6">Chilly ambience and the place is really nice to hangout or to just read books while sipping some coffee. The staffs are very friendly too, they definitely serve good food and good drinks!</h5>
         <h5 class="h5"><br>John Lee</h5>
         <h5 class="h6">Manager</h5>
         <img src="{{ asset('softui/assets/img/manager.png') }}" width="132px" height="132px">
@@ -194,108 +218,132 @@
 
   <section class="my-5 py-5">
     <div class="container">
-      <div class="row">
-        <div class="row justify-content-center text-center my-sm-5">
-          <div class="col-lg-6">
-            <h2 class="text-dark mb-0">UPCOMING EVENTS</h2>
-            <h3 class="text-primary text-gradient">Don't Miss Out On Our Coffee Delights</h3>
-          </div>
-        </div>
-      </div>
-
-      <div class="row align-items-center">
-        <div class="col-6 col-md-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <img src="{{ asset('softui/assets/img/poster 1.jpg') }}" width="265px" height="327px"></img> 
-              
+        <div class="row">
+            <div class="row justify-content-center text-center my-sm-5">
+                <div class="col-lg-6">
+                    <h2 class="text-dark mb-0">UPCOMING EVENTS</h2>
+                    <h3 class="text-primary text-gradient">Don't Miss Out On Our Coffee Delights</h3>
+                </div>
             </div>
-            
-          </div>
         </div>
 
-        <div class="col-6 col-md-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <img src="{{ asset('softui/assets/img/poster 2.jpg') }}" width="265px" height="327px"></img>  
-              
+        <div class="row align-items-center">
+            <div class="col-6 col-md-4">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Latte Art Workshop</h5>
+                        <p class="card-text">Join us for a hands-on workshop where you can learn the art of creating stunning latte designs. Perfect for coffee enthusiasts!</p>
+                        <img src="{{ asset('softui/assets/img/poster 1.jpg') }}" width="265px" height="327px"></img>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
 
-        <div class="col-6 col-md-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <img src="{{ asset('softui/assets/img/poster 3.jpg') }}" width="265px" height="327px"></img>  
-              
+            <div class="col-6 col-md-4">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Coffee Tasting Experience</h5>
+                        <p class="card-text">Explore a variety of coffee flavors from around the world in this guided tasting session. Discover your new favorite blend!</p>
+                        <img src="{{ asset('softui/assets/img/poster 2.jpg') }}" width="265px" height="327px"></img>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <div class="col-6 col-md-4">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Live Music Nights</h5>
+                        <p class="card-text">Enjoy live music performances by local artists every Friday night. Relax with a cup of coffee and good tunes.</p>
+                        <img src="{{ asset('softui/assets/img/poster 3.jpg') }}" width="265px" height="327px"></img>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
-  
+</section>
 
-  <footer class="footer pt-5 mt-5" style="background-color: #A37E2D">
-    
+
+
+<footer class="footer pt-5 mt-5" style="background-color: #A37E2D">
     <div class="container">
-      <div class="row">
-        <h6 style="color: white">ENJOY WELCOMING<br>ATMOSPHERE & MOMENTS<br>OF DELIGHT</h6>
-      </div>
-      <div class="row">
-        <div class="hr"></div>
-      </div>
-      <div class="row">
-        
-      </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h6 style="color: white; font-size: 1.5rem; font-weight: 500; margin-bottom: 0;">ENJOY WELCOMING<br>ATMOSPHERE & MOMENTS<br>OF DELIGHT</h6>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="hr" style="width: 100%; height: 1px; background-color: white;"></div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <ul class="list-inline" style="font-size: 1rem;">
+                    <li class="list-inline-item">
+                        <a href="#" style="color: white; text-decoration: none;">Home</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" style="color: white; text-decoration: none;">Menu</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" style="color: white; text-decoration: none;">Login</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" style="color: white; text-decoration: none;">Contact</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" style="color: white; text-decoration: none;">Events</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <a href="#" style="text-decoration: none;">
+                    <i class="fab fa-instagram" style="font-size: 2rem; color: white;"></i>
+                </a>
+            </div>
+        </div>
     </div>
-  </footer>
-
+</footer>
 
   <!--   Core JS Files   -->
   <script src="{{ asset('softui/assets/js/core/popper.min.js') }}" type="text/javascript"></script>
-  
+
   <script src="{{ asset('softui/assets/js/core/bootstrap.min.js') }}" type="text/javascript"></script>
-  
+
   <script src="{{ asset('softui/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-  
+
 
   <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
   <script src="{{ asset('softui/assets/js/plugins/countup.min.js') }}"></script>
-  
+
 
   <script src="{{ asset('softui/assets/js/plugins/choices.min.js') }}"></script>
 
   <script src="{{ asset('softui/assets/js/plugins/prism.min.js') }}"></script>
-  
+
   <script src="{{ asset('softui/assets/js/plugins/highlight.min.js') }}"></script>
-  
+
   <!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax -->
   <script src="{{ asset('softui/assets/js/plugins/rellax.min.js') }}"></script>
-  
+
   <!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
   <script src="{{ asset('softui/assets/js/plugins/tilt.min.js') }}"></script>
-  
+
   <!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices -->
   <script src="{{ asset('softui/assets/js/plugins/choices.min.js') }}"></script>
-  
+
 
   <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
   <script src="{{ asset('softui/assets/js/plugins/parallax.min.js') }}"></script>
-  
+
 
   <!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc -->
   <!--  Google Maps Plugin    -->
 
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
   <script src="{{ asset('softui/assets/js/soft-design-system.min.js?v=1.0.9') }}" type="text/javascript"></script>
-  
+
 
   <script type="text/javascript">
 
